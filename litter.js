2a0eefd0f7345fe2ad7b22458ba615e072a0cabe
Travis Reeder <treeder@gmail.com>
2023-04-12 02:38:21 +0000

    comments

--- litter.js
+++ litter.js
@@ -1,11 +1,12 @@
 import * as fs from 'fs';
 
+// Litterbox adds some special functions that the template can call, such as include.
 class LitterBox {
     constructor(data) {
         this.data = data
     }
     include(filename) {
-        console.log('INCLUDE', filename)
+        // console.log('INCLUDE', filename)
         return litter(filename, this.data)
     }
 }
@@ -20,7 +21,7 @@ function compile(templateStr) {
 
 export function litter(filename, data) {
     let code = fs.readFileSync(filename, 'utf8')
-    let runTemplate = compile(code) // todo cache these in production
-    let s = runTemplate(data, new LitterBox(data))
+    let tfunc = compile(code) // todo cache these in production
+    let s = tfunc(data, new LitterBox(data))
     return s
 }
