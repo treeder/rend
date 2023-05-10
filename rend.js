ad4f51050e052f9304120b4912ddfa84ae3103e4
Travis Reeder <treeder@gmail.com>
2023-05-10 12:02:11 -0700

    Update rend.js

--- rend.js
+++ rend.js
@@ -7,7 +7,7 @@ export class Rend {
         this.templates = {}
     }
 
-
+    // render returns a string of the rendered content
     async render(bodyFunc, d) {
         let o = this.options
         let b
@@ -17,7 +17,7 @@ export class Rend {
             let template =this.templates[bodyFunc] 
             if (!template) {
                 template = await import(path.join(process.cwd(), bodyFunc))
-                if(this.options.prod){
+                if(true ||this.options.prod){
                     this.templates[bodyFunc] = template
                 }
             } else {
@@ -34,6 +34,9 @@ export class Rend {
         return s
     }
 
+    // send 
+    // reply is a fastify reply object
+    // bodyFund can be a function or a string path to a module that exports a render function
     async send(reply, bodyFunc, d={}, opts={}) {
         reply.header('Content-Type', 'text/html')
         if(opts.headers){
@@ -44,4 +47,13 @@ export class Rend {
         return reply.send(await this.render(bodyFunc, d))
     }
 
+}
+
+// stringify is for passing objects as html atttributes. 
+// JSON.stringify doesn't work if your object has double and single quotes as it will end the string early.
+export function stringify(ob){
+    return JSON.stringify(ob).replaceAll(
+        '"',
+        '&quot;'
+      )
 }
