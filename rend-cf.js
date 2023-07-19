3c4f8129c3df6740690ea7aec71741c4b5f66cf6
Travis Reeder <treeder@gmail.com>
2023-07-19 13:22:51 -0700

    Update rend-cf.js

--- rend-cf.js
+++ rend-cf.js
@@ -1,12 +1,12 @@
-import path from 'node:path'
-import * as process from 'node:process';
+// import path from 'node:path'
+// import * as process from 'node:process';
 
 export class Rend {
 
     // options:
     //   - header, footer: template functions that will wrap around the content
     //   - prod (boolean): enable for performance enhancements such as cached templates
-    constructor(options={}) {
+    constructor(options = {}) {
         this.options = options
 
         // production cache of templates. default disabled. Set options.prod = true to enable.
@@ -20,13 +20,20 @@ export class Rend {
         }
         let o = this.options
         let b
-        if (typeof v === 'function') {
+        if (bodyFunc instanceof Promise) {
+            bodyFunc = await bodyFunc
+        }
+        if (typeof bodyFunc === 'string') {
+            b = bodyFunc
+        } else if ('render' in bodyFunc) {
+            b = bodyFunc.render(d)
+        } else if (typeof bodyFunc === 'function') {
             b = bodyFunc(d)
         } else {
-            let template =this.templates[bodyFunc] 
+            let template = this.templates[bodyFunc]
             if (!template) {
                 template = await import(bodyFunc)
-                if(o.prod){
+                if (o.prod) {
                     this.templates[bodyFunc] = template
                 }
             } else {
@@ -45,10 +52,10 @@ export class Rend {
     // send 
     // reply is a fastify reply object
     // bodyFund can be a function or a string path to a module that exports a render function
-    async send(reply, bodyFunc, d={}, opts={}) {
+    async send(reply, bodyFunc, d = {}, opts = {}) {
         reply.header('Content-Type', 'text/html')
-        if(opts.headers){
-            for (const h in opts.headers){
+        if (opts.headers) {
+            for (const h in opts.headers) {
                 reply.header(h, opts.headers[h])
             }
         }
