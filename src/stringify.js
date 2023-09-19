661b142172090daddce34019ed816300cc6849e4
Travis Reeder <treeder@gmail.com>
2023-09-19 06:04:56 -0700

    Handle strings better

--- src/stringify.js
+++ src/stringify.js
@@ -1,9 +1,15 @@
-
 // stringify is for passing objects as html atttributes. 
 // JSON.stringify doesn't work if your object has double and single quotes as it will end the string early.
 export function stringify(ob) {
-    return JSON.stringify(ob).replaceAll(
+    let s = ''
+    if (typeof ob === 'string') {
+        s = ob
+    } else {
+        s = JSON.stringify(ob)
+    }
+    s = s.replaceAll(
         '"',
         '&quot;'
     )
+    return s
 }
