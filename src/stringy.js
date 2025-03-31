1295f08f584a7bfe43f35c39ebfdb9dc7be32fdb
Travis Reeder <treeder@gmail.com>
2025-03-30 22:52:50 -0700

    Update stringy.js

--- src/stringy.js
+++ src/stringy.js
@@ -1,6 +1,7 @@
 // stringify is for passing objects as html atttributes. 
 // JSON.stringify doesn't work if your object has double and single quotes as it will end the string early.
 export function stringifyAttribute(ob) {
+    if(!ob) return ''
     let s = ''
     if (typeof ob === 'string') {
         s = ob
